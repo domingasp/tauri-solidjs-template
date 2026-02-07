daf2679963df3f4b29464331197a0fc36c5a6f26
Domingas Pleckauskas <d.pleck@outlook.com>
2026-02-07 09:07:51 +0000

    feat: add solidjs eslint plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginSolid from "eslint-plugin-solid/configs/typescript";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
@@ -13,6 +14,9 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
+
+  // eslint-plugin-solid type not compatible with eslint
+  eslintPluginSolid as never,
 
   // Prettier plugin should be last to override other rules
   eslintPluginPrettierRecommended,
