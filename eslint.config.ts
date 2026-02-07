20229ad6e917be12ac6809bce8fe806c66af5398
Domingas Pleckauskas <d.pleck@outlook.com>
2026-02-07 07:32:23 +0000

    feat: add ESLint and Prettier

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  globalIgnores(["src-tauri/**"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+
+  // Prettier plugin should be last to override other rules
+  eslintPluginPrettierRecommended,
+]);
