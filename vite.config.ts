8d17747418087399bfc77f266702d1fb326da620
Domingas Pleckauskas <d.pleck@outlook.com>
2026-02-07 13:03:47 +0000

    feat: add perfectionist eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,26 @@ import solid from "vite-plugin-solid";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
+// Vite options tailored for Tauri development and only applied in `tauri dev`
+// or `tauri build`
 export default defineConfig(async () => ({
-  plugins: [solid()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
+  // prevent Vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+
+  plugins: [solid()],
+  // tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
           host,
           port: 1421,
+          protocol: "ws",
         }
       : undefined,
+    host: host || false,
+    port: 1420,
+    strictPort: true,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
