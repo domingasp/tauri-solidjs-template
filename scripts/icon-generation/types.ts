ab06422eb514f15582867ae3e69dd91531652720
Domingas Pleckauskas <d.pleck@outlook.com>
2026-02-19 13:10:26 +0000

    refactor: prompt to ask if windows should have transparent bg

--- scripts/icon-generation/types.ts
+++ scripts/icon-generation/types.ts
@@ -1,3 +1,3 @@
-export type MacOSShape = "rounded-rectangle" | "squircle";
+export type BackgroundShape = "rounded-rectangle" | "squircle";
 
 export type Platform = "android" | "ios" | "macos" | "windows";
